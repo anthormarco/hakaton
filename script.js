7a9fe2fa5b986e869e96d2a7dab208e1d56d10a1
anthormarco <anthormarco@gmail.com>
2026-08-18 00:57:57 -0600

    se agrega el carrito otra vez

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 let productoSeleccionado = null;
 
+const productos = [
 
+    {
+        id: 1,
+
+        nombre: "Playera Gear Sport",
+
+        precio: 599,
+
+        imagen: "img/playera.jpg",
+
+        descripcion:
+            "Playera deportiva diseñada para ofrecer comodidad y libertad de movimiento durante tus entrenamientos.",
+
+        caracteristicas: [
+            "Tela transpirable",
+            "Secado rápido",
+            "Diseño deportivo",
+            "Disponible en varias tallas"
+        ]
+    },
+
+
+    {
+        id: 2,
+
+        nombre: "Short Deportivo",
+
+        precio: 499,
+
+        imagen: "img/short.jpg",
+
+        descripcion:
+            "Short ligero y cómodo para entrenamiento, gimnasio y actividades deportivas.",
+
+        caracteristicas: [
+            "Material ligero",
+            "Bolsillos laterales",
+            "Cintura ajustable",
+            "Tela transpirable"
+        ]
+    }
+
+];
 // ==========================
 // MOSTRAR PRODUCTO
 // ==========================
@@ -201,3 +244,4 @@ document.addEventListener(
 
     }
 );
+
